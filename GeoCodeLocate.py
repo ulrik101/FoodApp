a452a4246c596cb7a49561fa508a02e0e7cd61de
ulrik101 <42847517+ulrik101@users.noreply.github.com>
2019-11-19 18:28:52 -0800

    Made Object Oriented

--- GeoCodeLocate.py
+++ GeoCodeLocate.py
@@ -6,27 +6,27 @@ import requests
 import responses
 
 
+class Location:
+	def __init__(self):
+		self.gmapsKey = googlemaps.Client(key='AIzaSyAt0viCrbReQdXUR00mG4DPWucra5_xx8Y')
+		self.results = self.gmapsKey.geolocate()
+	def getLocation(self):
+		#accessing individual coords
+		lat = self.results["location"]["lat"]
+		lng = self.results["location"]["lng"]
+		coords = (lat,lng)
 
-gmapsKey = googlemaps.Client(key='AIzaSyAt0viCrbReQdXUR00mG4DPWucra5_xx8Y')
-
-results = gmapsKey.geolocate()
-
-#accessing individual coords
-lat = results["location"]["lat"]
-lng = results["location"]["lng"]
-coords = (lat,lng)
-
-print("current coordinates")
-print(coords)
-
-
+		return coords
+coord1=Location()
+test=coord1.getLocation()
+print(coord1.getLocation())
 
 #JSON TABLE EXAMPLE BELOW
 #https://maps.googleapis.com/maps/api/geocode/json?latlng=33.7867326,-117.8428536999999&key=AIzaSyAt0viCrbReQdXUR00mG4DPWucra5_xx8Y
 
-resultsReverse = gmapsKey.reverse_geocode(coords)
+#resultsReverse = gmapsKey.reverse_geocode(coords)
 
 
 #accessing a variable within json
-print("Nearby address")
-print(resultsReverse[0]["address_components"][0]["long_name"])
+#print("Nearby address")
+#print(resultsReverse[0]["address_components"][0]["long_name"])
