a96d56613ed78b4656aa7ef83dafb9fdde2a7e2e
ulrik101 <42847517+ulrik101@users.noreply.github.com>
2019-12-01 16:51:12 -0800

    Linted

--- RestaurantFilter.py
+++ RestaurantFilter.py
@@ -8,12 +8,14 @@ variables I want in results:
 'user_ratings_total'
 'vicinity' --like address
 
-gives back a dictionary of lists of dictionaries and now I am just parsing thorught that
+gives back a dictionary of lists of dictionaries and now
+I am just parsing thorught that
 """
 
 import time
 import operator
 from GeoCodeLocate import Location
+
 
 class Filter:
 
@@ -29,47 +31,61 @@ class Filter:
         self.sorted_scores = []
 
     def fill_places(self, ranging):
-
-        self.places_result.append(self.api_key.places_nearby(location = self.coordinates, radius = ranging, open_now = True, type = 'restaurant'))
+        self.places_result.append(self.api_key.places_nearby
+                                  (location=self.coordinates, radius=ranging,
+                                   open_now=True, type='restaurant'))
         count = 0
 
         while 'next_page_token' in self.places_result[count].keys():
-            #pprint.pprint(places_result[count]['next_page_token'])
-            
+            # pprint.pprint(places_result[count]['next_page_token'])
+
             count = count + 1
             time.sleep(3)
-            self.places_result.append(self.api_key.places_nearby(page_token = self.places_result[count-1]['next_page_token']))
+            self.places_result.append
+            (self.api_key.places_nearb
+             (page_token=self.places_result[count-1]['next_page_token']))
 
         for i in self.places_result:
             self.results_part.extend(i['results'])
 
-    def get_description (self, restaurant_number):
-        #create try catches for every space 
-        description = self.results_part[restaurant_number]['name'] + "\n"
-        description += "at: " + self.results_part[restaurant_number]['vicinity'] + "\n"
+    def get_description(self, restaurant_number):
+        # create try catches for every space
+        description = self.results_part[restaurant_number]
+        ['name'] + "\n"
+        description += "at: " + self.results_part
+        [restaurant_number]['vicinity'] + "\n"
         try:
-            description += "price level: " + str(self.results_part[restaurant_number]['price_level']) + "\n"
-        except:
+            description += "price level: " + str(self.results_part
+                                                 [restaurant_number]
+                                                 ['price_level']) + "\n"
+        except Exception:
             description += "no price level stated\n"
         finally:
-            description += "has a rating of " + str(self.results_part[restaurant_number]['rating']) + " stars\n"
-            description += "with " + str(self.results_part[restaurant_number]['user_ratings_total'])
-            
+            description += "has a rating of " + str(self.results_part
+                                                    [restaurant_number]
+                                                    ['rating']) + " stars\n"
+            description += "with " + str(self.results_part
+                                         [restaurant_number]
+                                         ['user_ratings_total'])
+
             if self.results_part[restaurant_number]['user_ratings_total'] == 1:
                 description += str(" review")
             else:
                 description += str(" reviews")
             return description
 
-    def get_score (self, restaurant_number):
+    def get_score(self, restaurant_number):
 
-        # the price level is a missing value the try catch block will try to account for that 
+        # the price level is a missing value the try catch
+        # block will try to account for that
         try:
-            useful_price_level = self.results_part[restaurant_number]['price_level']
-        except:
+            useful_price_level = self.results_part[
+                                      restaurant_number]['price_level']
+        except Exception:
             useful_price_level = 5
         finally:
-            amount_ratings = self.results_part[restaurant_number]['user_ratings_total']
+            amount_ratings = self.results_part[restaurant_number][
+                                  'user_ratings_total']
             star_rating = self.results_part[restaurant_number]['rating']
             score = (amount_ratings * star_rating) / (useful_price_level + 1)
             return score
@@ -85,12 +101,13 @@ class Filter:
         # sorted score is now a list of tuples
         # sorting based on scores rating formula created
 
-        self.sorted_scores = sorted(self.scores.items(), key=operator.itemgetter(1), reverse = True)
+        self.sorted_scores = sorted(self.scores.items(),
+                                    key=operator.itemgetter(1),
+                                    reverse=True)
         for i in range(len(self.sorted_scores)):
             identification = self.sorted_scores[i][0]
-
             count = i + 1
-            print(count,".")
+            print(count, ".")
             print(self.descriptions[identification])
             print("has a rating of ", self.sorted_scores[i][1])
             print()
@@ -106,19 +123,23 @@ class Filter:
             try:
                 if self.results_part[i]['price_level'] == 0:
                     results_part2.append(self.results_part[i])
-            except:
+            except Exception:
                 pass
         if len(results_part2) != 0:
             self.results_part = results_part2
             self.fill_dictionaries()
             self.print_all_as_sorted()
         else:
-            print("There were no restaurants in your radius with the price level being 0")
-        
-    def rank (self, ranging):
+            message = "There were no restaurants in radius"
+            message += " with the price level being 0"
+            print(message)
+
+    def rank(self, ranging):
+
         self.fill_places(ranging)
         self.fill_dictionaries()
         self.print_all_as_sorted()
+
         return len(self.sorted_scores) != 0
 
 
@@ -128,5 +149,3 @@ class Filter:
 # https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value
 
 # https://stackoverflow.com/questions/50704611/next-page-token-google-places
-
-
--- RestaurantRanker.py
+++ RestaurantRanker.py
@@ -1,13 +1,20 @@
 from RestaurantFilter import Filter
 
 ranker = Filter()
-ranging = int(input("What is the range you would like to view restaurants within (in meters)? "))
+rangeString = "What is the range you would like to view"
+rangeString += " restaurants within (in meters)? "
+ranging = int(input(rangeString))
 any_restaurants = ranker.rank(ranging)
 
 if not any_restaurants:
     print("There were no restaurants in the area specified")
 else:
-    choice = input("Would you like to find food from the previous group of restaurants with the lowest price level possible being 0 (type \'yes\' to see)? ")
+    messageString = "Would you like to find food"
+    messageString += " from the previous group of"
+    messageString += " restaurants with the lowest"
+    messageString += " price level possible being 0"
+    messageString += " (type \'yes\' to see)? "
+    choice = input(messageString)
     if choice == "yes":
         ranker.only_free_food()
     else:
